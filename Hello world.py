b0386861ef5c74f88872250bc6f6f70830fbea98
Hyeseok-0828 <hyeseok.nam@kingsway.school.nz>
2024-07-25 11:11:31 +1200

    Add files via upload
    My first phthon tutorials

---
+++ Hello world.py
@@ -0,0 +1,7 @@
+# Hello world
+# Author: Hyeseok Nam
+# Date: 2024 july 23
+# version: 1
+
+# Display the greeting "Hello world!" in the terminal
+print("Hello world!")
---
+++ Variables.py
@@ -0,0 +1,32 @@
+# Name: Variables
+# Author: Hyeseok Nam
+# Date: 2024 July 23
+# Version: 1
+
+# TODO: Show the various ways varibles.
+
+# This is comment using a hashtag (#)
+# Various varible names
+# A variable is a container to hold a value
+first_prime = 2 # varible with a number
+another_number = 16
+nz_capital = "Wellington!" # varible with a string (text)
+djsk = "Hello " # Varibles can be called anything
+
+# Display the value of the varible 
+#print(first_prime)
+#print(nz_capital)
+
+# Add varible to each other (concatenation)
+print(djsk + nz_capital)
+
+# Do calculation with varibles
+add = 2 + 15
+print(add)
+
+add_var = first_prime + another_number # adding variables together
+print(add_var)
+
+#Divide varibles
+divide = another_num / first_prime 
+print(divide)
