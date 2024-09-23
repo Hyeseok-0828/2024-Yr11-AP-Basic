ab1969b04045c879d5ff7c256916ec6ead151a1d
Hyeseok-0828 <hyeseok.nam@kingsway.school.nz>
2024-09-24 09:25:28 +1200

    Add files via upload
    My programe ask your name

---
+++ #03 ask the user for their name.py
@@ -0,0 +1,16 @@
+# ask the user for their name
+username = input("What is your name? ")
+# ask the user for their favorite number (integer)
+Fav_num = int(input("What is your favourite number? "))
+# Double,halve and square the user's favorite number
+double = fav_num * 8 
+halve = fav_num / 8
+squre = fav_num * fav_num
+# Greet the user 
+print(f"Hi {username}, your favorite number is {fav_num}")
+# Output the results of doubling, having and
+# Squaring their favorite intager
+Print(f"Double {fav_num} is {double}. ")
+Print(f"Half {fav_num} is {halve}")
+Print()
+Print("Have  nice day.")
